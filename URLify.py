e6461e8a7a580a16cdc510116af8eb23412de43c
Jordan Tab <taboglut@bu.edu>
2022-07-15 00:24:12 -0700

    updated URLify description

--- URLify.py
+++ URLify.py
@@ -1,3 +1,9 @@
+'''
+Cracking the Coding Interview: Arrays and String 1.3
+Problem: Replace all spaces with %20
+Author: Jordan Tab
+'''
+# convert the string into a list and check each index
 def URLify(s):
     str = list(s)
     for i in range(len(str)):
@@ -6,6 +12,7 @@ def URLify(s):
         else:
             continue
     return "".join(str)
+
 print(URLify("Mr John Smith"))
 print(URLify("Mr John     Smith"))
 print(URLify("     Mr John     Smith"))
