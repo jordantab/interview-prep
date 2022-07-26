71c7f297f47f53c698a8236838d0ff93957875e9
Jordan Tab <taboglut@bu.edu>
2022-07-25 23:26:37 -0700

    Solved palindrome_permutation

--- Cracking the Coding Interview/Arrays and Strings/palindrome_permutation.py
+++ Cracking the Coding Interview/Arrays and Strings/palindrome_permutation.py
@@ -1,21 +1,30 @@
-def remove(string):
-    return string.replace(" ","")
-def palindrome_permutation(s):
-    str = remove(s)
-    hashT = {}
-    for char in str:
-        hashT[char] = 0
-    for char in str:
-        hashT[char] += 1
-    even_count = 0
-    for char in hashT:
-        if hashT[char] % 2 == 0:
-            even_count += 1
-    print(even_count)
-    if (even_count == len(hashT)) or (even_count == (len(hashT) - 1)):
-        return True
-    else:
+'''
+Cracking the Coding Interview: Arrays and String 1.4
+Problem: Given a string, write a function to check if it is a permutation of a palinfrome. A palindrome is a word or phrase that is the same forwards and backwards
+Author: Jordan Tab
+'''
+
+def palindrome_permutation(string):
+    '''
+    A palindrome can have at most one letter with an odd number of occurence. Since order doesn't matter for permutation, check total odd_count
+    '''
+    s = string.lower()
+    s = s.replace(" ","")
+    d = {}
+    for char in s:
+        if char not in d:
+            d[char] = 1
+        else:
+            d[char] += 1
+    odd_count = 0
+    for char in d:
+        if d[char] % 2 == 1:
+            odd_count += 1
+    if odd_count > 1:
         return False
+    else:
+        return True
+
 print(palindrome_permutation("ajbdfj"))
 print(palindrome_permutation("tact coa"))
 print(palindrome_permutation("racecar"))
