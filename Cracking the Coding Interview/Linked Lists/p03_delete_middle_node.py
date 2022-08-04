6669152c71cec8d75da865bb4e742b9e11bf1bab
Jordan Tab <taboglut@bu.edu>
2022-08-04 15:02:39 -0700

    Implemented delete_middle_node solution

--- Cracking the Coding Interview/Linked Lists/p03_delete_middle_node.py
+++ Cracking the Coding Interview/Linked Lists/p03_delete_middle_node.py
@@ -9,6 +9,10 @@ a singly linked list, given only access to that node
 Author: Jordan Tab
 '''
 
+from calendar import c
+from pytest import PytestUnhandledThreadExceptionWarning
+
+
 class Node:
     '''
     Node class
@@ -30,3 +34,31 @@ fourth = Node('d',fifth)
 third = Node('c',fourth)    
 second = Node('b',third)
 first = Node('a',second)
+
+# function to print the linked list
+def print_list(head):
+    temp = head
+
+    while (temp):
+        print(temp.data, end=" ")
+        temp = temp.next
+    print()
+print_list(first)
+
+def delete_middle_node(head,node):
+    '''
+    deletes the given node from the linked list
+
+    logic: only have access to the current node, update it with the contents of the next node and delete the next node since we can't
+    access the previous node
+    '''
+    temp = head
+    while temp.data != node:
+        temp = temp.next
+    if temp.data == node:
+        temp.data = temp.next.data
+        temp.next = temp.next.next
+    return head
+    
+res = delete_middle_node(first,'c')
+print_list(res)
