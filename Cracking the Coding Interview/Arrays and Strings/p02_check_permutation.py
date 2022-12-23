cb795e68d6ae00458659d5e5a36a6093c849b774
Jordan Tab <taboglut@bu.edu>
2022-12-22 19:45:27 -0500

    Simple changes to old solutions

--- Cracking the Coding Interview/Arrays and Strings/p02_check_permutation.py
+++ Cracking the Coding Interview/Arrays and Strings/p02_check_permutation.py
@@ -16,3 +16,41 @@ def checkperm(s1,s2):
 
 print(checkperm("abcced","deccba"))
 print(checkperm("abccefeed","deccba"))
+
+
+def check_permutation(s1,s2):
+    '''uses hash tables and not built in functions'''
+    hashT1 = {}
+    hashT2 = {}
+
+    for letter in s1:
+        if letter not in hashT1:
+            hashT1[letter] = 1
+        else:
+            hashT1[letter] += 1
+
+    for letter in s2:
+        if letter not in hashT2:
+            hashT2[letter] = 1
+        else:
+            hashT2[letter] += 1
+
+    len1 = len(hashT1)
+    len2 = len(hashT2)
+
+    if len1 != len2:
+        return False
+    else:
+        for letter in hashT1:
+            if letter not in hashT2:
+                return False
+            else:
+                if hashT1[letter] != hashT2[letter]:
+                    return False
+    return True
+
+print(check_permutation('awasd', 'dasaw'))
+print(check_permutation('awasd', 'dasawf'))
+print(check_permutation('awasd', 'dasac'))
+print(check_permutation('awasd', 'daaaw'))
+
